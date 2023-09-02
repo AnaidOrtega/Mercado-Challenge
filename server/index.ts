49614435051cb669012a144d3701e0d2d70c0d41
AnaidOrtega <itzel.ortega9819@hotmail.com>
2023-09-01 23:29:13 -0600

    First Commit

---
+++ server/index.ts
@@ -0,0 +1,20 @@
+import express, { NextFunction, Request, Response } from 'express'
+
+const app = express();
+
+app.get("/", (req: Request, res: Response, next: NextFunction): void => {
+    try {
+        res.send("Hello World");
+    } catch (error) {
+        next(error);
+    }
+})
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`)
+})
+
+
+
